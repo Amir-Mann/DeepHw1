07c560e072a0a286d6b9ac977eae8aea30dddf2f
Roei Bloch <roei.bloch@campus.technion.ac.il>
2023-04-01 17:00:12 +0300

    finish part 1

--- hw1/answers.py
+++ hw1/answers.py
@@ -9,8 +9,19 @@ math (delimited with $$).
 # Part 1 answers
 
 part1_q1 = r"""
-**Your answer:**
+**Your answer: 1. False  2. False  3. True**
 
+**explanation**
+1.The in-sa0mple error a determnistic value calculated on the training dataset (only), 
+  independed of the test dataset. 
+  the test dataset helps us to estimate the out-of-sample error. 
+
+2. we can split out dataset into a zero size training dataset, and full size test dataset.
+   such split would make any model equally good. and so training would not change our initial guess.
+   and no learning would be done at all.
+   
+3. in cross validation we change and train our training a and validation split to get a better optimization for our hyperparameters
+   using the test set in our cross validation would contaminate our out-of-sample estimation. so we should not use it.
 
 Write your answer using **markdown** and $\LaTeX$:
 ```python
@@ -22,8 +33,11 @@ An equation: $e^{i\pi} -1 = 0$
 """
 
 part1_q2 = r"""
-**Your answer:**
+**Your answer: Not justified**
 
+by using the test set to determine the $\lambda$ we damage the ability to estimate our model's out-of-sample error.
+because we choose the $\lambda$ that will produce better results specificly on the test set and therefoe the test seete will no longer
+new unseen data from the theoretical distribution.
 
 Write your answer using **markdown** and $\LaTeX$:
 ```python
